8ae73e1b20e21c8bc06356d558dd98b2679ecddc
Sofia Larsson Tibell <sl224aw@student.lnu.se>
2022-12-06 08:43:28 +0100

    Lits on Profile

--- Litter/src/app/profile/profile.component.ts
+++ Litter/src/app/profile/profile.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+type userMessage = {
+  message: string,
+  date: string,
+  time: string
+};
 
 @Component({
   selector: 'app-profile',
@@ -6,15 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  userMessages: Array<userMessage> = new Array();
   username = "Marie"
   catType = "Burmese"
   friends = 6
-  messages = 16
+  messages = this.userMessages.length
   imgSrc = "assets/img/cat.png"
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+    this.userMessages.push({
+      message: "First message",
+      date: "2022.12.11",
+      time: "12.01"
+    })
+    this.userMessages.push({
+      message: "Second message",
+      date: "2022.12.11",
+      time: "13.55"
+    })
+    this.userMessages.push({
+      message: "Third message",
+      date: "2022.12.11",
+      time: "13.59"
+    })
+    this.messages = this.userMessages.length
   }
 
+  onClick(): void {
+    this.router.navigate(['./', 'sandbox']);
+  }
 }
